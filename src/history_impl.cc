e9bb00fd7fd1ed4b3acd78ee5d2a93afac1cadda
Andreas Fett <a.fett@gmx.de>
2014-07-06 10:31:52 +0200

    update copyright

--- src/history_impl.cc
+++ src/history_impl.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012, 2013 Andreas Fett.
+ * Copyright (c) 2012, 2013, 2014 Andreas Fett.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
